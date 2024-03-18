c51bed22b55df7951aaf9206dcd465c408f7465e
GoGordini <eleonora.tubio@gmail.com>
2024-03-17 23:36:09 -0300

    Mail confirmacion de compra

--- src/services/tickets.service.js
+++ src/services/tickets.service.js
@@ -1,5 +1,6 @@
 import  TicketManagerRepository  from '../repositories/tickets.repository.js';
 import { TicketManager } from '../dao/factory.js';
+import {sendEmail} from "../services/mail.services.js" 
 const ticketManager = new TicketManager();
 const ticketManagerRepository= new TicketManagerRepository(ticketManager);
 
@@ -12,5 +13,30 @@ const ticketManagerRepository= new TicketManagerRepository(ticketManager);
         "purchaser": user.email
     }
     const result = await ticketManagerRepository.saveRepository(ticket);
+    const purchaseEmail = {
+        subject: 'Gracias por su compra en Happy Skin!',
+        html: `<!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Producto Eliminado</title>
+        </head>
+        <body>
+        
+            <div>               
+               <p>Estimado ${user.first_name},</p> 
+               <p>Gracias por su compra por $ ${totalPrice}.</p>
+                <p>Saludos cordiales,</p>
+                <p>Happy Skin. </p>
+            </div>        
+        </body>
+        </html>`
+    };
+        await sendEmail({
+            to: user.email,
+            subject: purchaseEmail.subject,
+            html: purchaseEmail.html})
+        
     return result;
 }
